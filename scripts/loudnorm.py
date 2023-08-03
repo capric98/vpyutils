f9832802171025484fd7cf59fc50b1fc8fccf6c3
capric98 <42015599+capric98@users.noreply.github.com>
2023-08-03 15:05:23 -0500

    add loudnorm.py

---
+++ scripts/loudnorm.py
@@ -0,0 +1,72 @@
+#!/usr/local/env python3
+# coding: utf-8
+import argparse
+import json
+import os
+import re
+import shutil
+import subprocess
+
+
+__FFMPEG__ = shutil.which("ffmpeg")
+
+
+def _check_env():
+    if not __FFMPEG__:
+        print("ffmpeg not installed!")
+        exit(1)
+
+def loudnorm(input_fn, output_fn, args):
+    print(f"Processing \"{input_fn}\"...")
+
+    ex = subprocess.run([
+        __FFMPEG__, "-hide_banner", "-loglevel", "info", "-nostats",
+        "-i", input_fn,
+        "-vn", "-af", "loudnorm=print_format=json", "-f", "null", output_fn,
+    ], capture_output=True)
+
+    stderr = ex.stderr.decode("utf-8")
+    r = re.search(r"\[Parsed_loudnorm.*?\]", stderr)
+    if not r:
+        print("ffmpeg gives unexpected output as:")
+        for line in stderr.splitlines(): print(f" > {line}")
+        return
+    else:
+        measure = json.loads(stderr[r.end():])
+        # print(measure)
+
+        mi      = measure["input_i"]
+        mlra    = measure["input_lra"]
+        mtp     = measure["input_tp"]
+        mthresh = measure["input_thresh"]
+
+        subprocess.run([
+            __FFMPEG__, "-hide_banner", "-loglevel", "info", "-nostats",
+            "-i", input_fn,
+            "-af", f"loudnorm=I={args.LUFS}:LRA={args.LRA}:TP={args.TP}:measured_I={mi}:measured_LRA={mlra}:measured_TP={mtp}:measured_thresh={mthresh}",
+            "-c:v", "copy", "-c:a", args.encoder, "-ab", args.bitrate, "-y", output_fn,
+        ], capture_output=True)
+
+
+if __name__=="__main__":
+    _check_env()
+
+    parser = argparse.ArgumentParser(
+        prog="loudnorm.py",
+        description="loudness normalization",
+        epilog="_(:з」∠)_",
+    )
+
+    parser.add_argument("video_fn", type=str, nargs="+")
+    parser.add_argument("-i", "--LUFS", type=float, help="loudness target", default=-18.0)
+    parser.add_argument("-l", "--LRA", type=float, help="loudness range", default=7.0)
+    parser.add_argument("-t", "--TP", type=float, help="true peak loudness", default=-1.0)
+    parser.add_argument("-e", "--encoder", type=str, help="audio encoder", default="aac")
+    parser.add_argument("-b", "--bitrate", type=str, help="audio bitrates", default="192k")
+
+    args = parser.parse_args()
+    # print(args)
+
+    for fn in args.video_fn:
+        ps = os.path.splitext(fn)
+        loudnorm(fn, ps[0]+".norm"+ps[1], args)
